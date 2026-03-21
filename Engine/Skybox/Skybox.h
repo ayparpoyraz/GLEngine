3b2fd3a5cd3c50e7fbda11a17aaadcdd3153d259
Aypar Poyraz İnhanlı <ayparcode@gmail.com>
2026-03-21 18:29:31 +0300

    Todo : Skybox

---
+++ Engine/Skybox/Skybox.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <vector>
+
+
+class Skybox
+{
+public:
+	Skybox(unsigned int textureID);
+	
+	
+
+private:
+
+};
+
